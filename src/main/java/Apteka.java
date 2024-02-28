a32c220adee9c8474c2983db0bc425869d5fb763
Adam <adas.j.wojcik@gmail.com>
2024-02-28 10:08:49 +0100

    Dodana Apteka

---
+++ src/main/java/Apteka.java
@@ -0,0 +1,75 @@
+public class Apteka {
+        String Kraj;
+        String Woj;
+        String Miasto;
+        String Ulica;
+        String Budynek;
+        String Lokal;
+        String KodPoczt;
+        String NumerTel;
+
+    public String getKraj() {
+        return Kraj;
+    }
+
+    public void setKraj(String kraj) {
+        Kraj = kraj;
+    }
+
+    public String getWoj() {
+        return Woj;
+    }
+
+    public void setWoj(String woj) {
+        Woj = woj;
+    }
+
+    public String getMiasto() {
+        return Miasto;
+    }
+
+    public void setMiasto(String miasto) {
+        Miasto = miasto;
+    }
+
+    public String getUlica() {
+        return Ulica;
+    }
+
+    public void setUlica(String ulica) {
+        Ulica = ulica;
+    }
+
+    public String getBudynek() {
+        return Budynek;
+    }
+
+    public void setBudynek(String budynek) {
+        Budynek = budynek;
+    }
+
+    public String getLokal() {
+        return Lokal;
+    }
+
+    public void setLokal(String lokal) {
+        Lokal = lokal;
+    }
+
+    public String getKodPoczt() {
+        return KodPoczt;
+    }
+
+    public void setKodPoczt(String kodPoczt) {
+        KodPoczt = kodPoczt;
+    }
+
+    public String getNumerTel() {
+        return NumerTel;
+    }
+
+    public void setNumerTel(String numerTel) {
+        NumerTel = numerTel;
+    }
+}
+
