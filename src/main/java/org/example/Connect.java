9c82f5963a7b0c97d07d2e51d63ec06fb6b4de29
Radoslaw <korepetrycjerd@proton.me>
2025-01-10 19:31:08 +0100

    przygotowanie pod baze danych

---
+++ src/main/java/org/example/Connect.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Connect {
+    private static final String JDBC_URL = "jdbc:h2:./database/apteka";
+    private static final String JDBC_USER = "sa";
+    private static final String JDBC_PASSWORD = "";
+
+    public static void main (String[] args){
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD)){
+            System.out.println("Połączenie z baza danych zostało nawiązane");
+            // przykłaodwy kod do utowrzenia tabeli 
+            String createTableSQL = "CREATE TABLE IF NOT EXISITS LEKI("+
+            "ID INT AUTO_INCREMENT PRIMARY KEY)";
+            Statement statement = connection.createStatement();
+            statement.execute(createTableSQL);
+            System.out.println("Tabela zostałą utworzona");
+
+        } catch (Exception e) {
+            e.printStackTrace();        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,38 @@
 package org.example;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class Main {
 
     public static void main(String[] args) {
 
+
+        JFrame frame = new JFrame("Pierwsze okno");
+        JButton button = new JButton("Zamknij");
+
+        button.addActionListener( new ActionListner() {
+            @Override
+            public void actionPerfomed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        frame.add(button);
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(640, 300);
+        frame.setVisible(true);
+
         LoginManager loginManager = new LoginManager("KlienciMD5.txt");
         loginManager.loginWithStaticData();
+
+        
+
+
+
 
 
     }
