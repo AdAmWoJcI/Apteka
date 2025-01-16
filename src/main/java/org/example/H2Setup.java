5c5304430b8cd8be759c7d3311de90c62f27b5c2
Radek <radoslaw.danielkiewicz@pollub.edu.pl>
2025-01-16 08:58:47 +0100

    zmiany z laptopa z baza danych (działąjaca baza danych)

---
+++ src/main/java/org/example/H2Setup.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class H2Setup {
+    private static final String JDBC_URL = "jdbc:h2:./database/apteka";
+    private static final String JDBC_USER = "sa";
+    private static final String JDBC_PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+    }
+
+    public static void main(String[] args) {
+        try (Connection connection = getConnection()) {
+            System.out.println("Połączenie z bazą danych H2 działa poprawnie!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/org/example/Klient.java
+++ src/main/java/org/example/Klient.java
@@ -1,19 +1,19 @@
 package org.example;
 
 public class Klient extends User {
-    private String numerRecepty;
+    private String pesel;
 
-    public Klient(String FN, String LN, String NR) {
+    public Klient(String FN, String LN, String pesel) {
         super(FN, LN);
-        this.numerRecepty = NR;
+        this.pesel = pesel;
     }
 
     public Klient(String FN, String LN) {
         super(FN, LN);
     }
 
-    public Klient(String firstName, String lastName, String privince, String town, String postCode, String street, String houseNumber, String flatNumber, String phone, String email, String email_ver, String password, String created_at, String numerRecepty) {
+    public Klient(String firstName, String lastName, String privince, String town, String postCode, String street, String houseNumber, String flatNumber, String phone, String email, String email_ver, String password, String created_at, String pesel) {
         super(firstName, lastName, privince, town, postCode, street, houseNumber, flatNumber, phone, email, email_ver, password, created_at);
-        this.numerRecepty = numerRecepty;
+        this.pesel = pesel;
     }
 }
